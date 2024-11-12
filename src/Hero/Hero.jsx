af51ddaddbb9f529f2e691b30485e1609a1cc610
AndyMiguel <user@Miguels-MacBook-Pro.local>
2024-11-12 15:50:45 +0100

    did the hero button

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -7,15 +7,28 @@ const Hero = () => {
   return (
     
     <>
-    
+    {/* hero box with image */}
        <div className='heroBox'> 
             <h1>High-Quality Business <br />Solutions</h1>
             <h3>Our company provides various services and solutions for <br /> businesses worldwide.</h3>
+          <div className='buttonAndIcon'>
             <button>Contact US</button>
-           <span>
+           <div>
             <PiPlayCircleDuotone className='icon'/>
-           </span>
+           </div>
+           </div>  
         </div>
+        {/* second layer with numbers */}
+
+        <div className='numberContainer'>
+            <ol>
+                <li>100 <br />PROJECTS ANNUALLY</li>
+                <li>45<br />AWARDS</li>
+                <li>98%<br />POSIVTIVE REWARDS </li>
+                <li>147K <br />HAPPY CUSTOMERS</li>
+            </ol>
+        </div>
+
       
     </>
   )
